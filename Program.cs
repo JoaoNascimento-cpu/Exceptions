4acb72088ade9a30584940a7aef74f8e446942c5
João Nascimento <jhonyzito4gb@gmail.com>
2022-06-20 20:13:02 -0300

    exception dateTime

--- Program.cs
+++ Program.cs
@@ -6,7 +6,57 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var arr = new int[3];
+
+            try
+            {    
+                // for (var index = 0; index < 10; index++)
+                // {
+                //     Console.WriteLine(arr[index]);
+                // }
+                Cadastrar("");
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Não encontrei o índice na lista");
+            }
+            catch (MinhaException ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.quandoAconteceu);
+                Console.WriteLine("Exceção customizada");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Algo deu errado");
+            }
+            finally
+            {
+                
+            }
+
+        }
+
+        private static void Cadastrar(string texto)
+        {
+            if (string.IsNullOrEmpty(texto))
+            {
+                throw new MinhaException(DateTime.Now);
+            }
+        }
+
+        public class MinhaException : Exception
+        {
+            public MinhaException(DateTime date)
+            {
+                quandoAconteceu = date;
+            }
+            public DateTime quandoAconteceu{get; set;}
         }
     }
 }
